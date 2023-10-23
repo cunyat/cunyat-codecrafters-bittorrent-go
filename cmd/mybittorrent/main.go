325ec736bb5d33f4270670e60546889c66160659
Ramon Cuñat <ramon.cunat@deliverea.com>
2023-10-23 07:33:00 +0200

    parse bencoded lists

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -11,42 +11,59 @@ import (
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
-    if bencodedString[0] == 'i' {
-        if len(bencodedString) <= 2 || bencodedString[len(bencodedString)-1] != 'e' {
-            return "", fmt.Errorf("unexpected integer format (%s)", bencodedString)
-        }
-
-        digits := bencodedString[1:len(bencodedString)-1]
-        value, err := strconv.Atoi(digits)
-        if err != nil {
-            return nil, err
-        }
-
-        return value, nil
-
-    }
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		firstColonIndex := strings.IndexByte(bencodedString, ':')
-
-		if firstColonIndex == -1 {
-			return "", fmt.Errorf("missing separator character")
-		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
+func decodeBencodeValue(str string) (interface{}, string, error) {
+	if strings.HasPrefix(str, "i") && len(str) > 2 {
+		end := strings.IndexByte(str, 'e')
+		digits := str[1:end]
+		value, err := strconv.Atoi(digits)
 		if err != nil {
-			return "", err
+			return nil, "", err
 		}
 
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else {
-		return "", fmt.Errorf("Only strings and integers are supported at the moment")
+		return value, str[end:], nil
 	}
+
+	if strings.HasPrefix(str, "l") {
+		var items []interface{}
+		var value interface{}
+		var err error
+		var rest = str[1:]
+
+		for {
+			value, rest, err = decodeBencodeValue(rest)
+			if err != nil {
+				return nil, "", err
+			}
+
+			items = append(items, value)
+			if strings.HasPrefix(rest, "e") {
+				return items, rest[1:], nil
+			}
+		}
+	}
+
+	if unicode.IsDigit(rune(str[0])) {
+		sep := strings.IndexByte(str, ':')
+
+		if sep == -1 {
+			return nil, "", fmt.Errorf("missing string separator character")
+		}
+
+		length, err := strconv.Atoi(str[:sep])
+		if err != nil {
+			return nil, "", fmt.Errorf("could not parse string length: %w", err)
+		}
+
+		end := sep + 1 + length
+		return str[sep+1 : end], str[end:], nil
+	}
+
+	return nil, "", fmt.Errorf("unexpected bencoded value format (%s)", str)
+}
+
+func decodeBencode(bencodedString string) (interface{}, error) {
+	value, _, err := decodeBencodeValue(bencodedString)
+	return value, err
 }
 
 func main() {
