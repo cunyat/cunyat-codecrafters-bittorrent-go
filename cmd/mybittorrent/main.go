67e505649a4088bda3b89f1753742fb369be65c6
Ramon Cuñat <ramon.cunat@deliverea.com>
2023-10-27 07:45:33 +0200

    parse dictionary in bencode format

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,77 +1,123 @@
 package main
 
 import (
-	// Uncomment this line to pass the first stage
 	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
 	"unicode"
-	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
-func decodeBencodeValue(str string) (interface{}, string, error) {
-	if strings.HasPrefix(str, "i") && len(str) > 2 {
-		end := strings.IndexByte(str, 'e')
-		digits := str[1:end]
-		value, err := strconv.Atoi(digits)
+// decodeString parses a string value in bencode format
+// assumes that the first byte in buf is a digit
+func decodeString(buf string) (string, string, error) {
+	sep := strings.IndexByte(buf, ':')
+	if sep == -1 {
+		return "", "", fmt.Errorf("missing string separator character")
+	}
+
+	length, err := strconv.Atoi(buf[:sep])
+	if err != nil {
+		return "", "", fmt.Errorf("could not parse string length: %w", err)
+	}
+
+	end := sep + 1 + length
+	return buf[sep+1 : end], buf[end:], nil
+}
+
+// decodeInteger parses an integer value in bencode format
+// assumes that the first byte in buf is an integer identifier 'i'
+func decodeInteger(buf string) (int, string, error) {
+	end := strings.IndexByte(buf, 'e')
+	if end == -1 {
+		return 0, "", fmt.Errorf("could not find integer end character")
+	}
+
+	digits := buf[1:end]
+	value, err := strconv.Atoi(digits)
+	if err != nil {
+		return 0, "", err
+	}
+
+	return value, buf[end+1:], nil
+}
+
+// decodeList parses a list of values in bencode format
+// assumes that the first byte in buf is a list identifier 'l'
+func decodeList(buf string) ([]interface{}, string, error) {
+	var items = []interface{}{}
+	var value interface{}
+	var err error
+	var rest = buf[1:]
+
+	for !strings.HasPrefix(rest, "e") {
+		value, rest, err = decodeBencodeValue(rest)
 		if err != nil {
 			return nil, "", err
 		}
 
-		return value, str[end+1:], nil
+		items = append(items, value)
 	}
 
-	if strings.HasPrefix(str, "l") {
-		var items = []interface{}{}
-		var value interface{}
-		var err error
-		var rest = str[1:]
-
-		for !strings.HasPrefix(rest, "e") {
-			value, rest, err = decodeBencodeValue(rest)
-			if err != nil {
-				return nil, "", err
-			}
-
-			items = append(items, value)
-		}
-
-		return items, rest[1:], nil
-	}
-
-	if unicode.IsDigit(rune(str[0])) {
-		sep := strings.IndexByte(str, ':')
-
-		if sep == -1 {
-			return nil, "", fmt.Errorf("missing string separator character")
-		}
-
-		length, err := strconv.Atoi(str[:sep])
-		if err != nil {
-			return nil, "", fmt.Errorf("could not parse string length: %w", err)
-		}
-
-		end := sep + 1 + length
-		return str[sep+1 : end], str[end:], nil
-	}
-
-	return nil, "", fmt.Errorf("unexpected bencoded value format (%s)", str)
+	return items, rest[1:], nil
 }
 
-func decodeBencode(bencodedString string) (interface{}, error) {
-	value, _, err := decodeBencodeValue(bencodedString)
+// decodeDictionary parses a dictionary in bencode format
+// assumes that the first byte in buf is a dictionary identifier 'd'
+func decodeDictionary(buf string) (map[string]interface{}, string, error) {
+	dict := map[string]interface{}{}
+	rest := buf[1:]
+	for !strings.HasPrefix(rest, "e") {
+		// dictionary key must be a string
+		key, r, err := decodeString(rest)
+		if err != nil {
+			return nil, "", fmt.Errorf("could not decode dictionary key, maybe not an string: %w", err)
+		}
+		rest = r
+		value, r, err := decodeBencodeValue(rest)
+		if err != nil {
+			return nil, "", fmt.Errorf("error parsing decode value for key '%s': %w", key, err)
+		}
+
+		dict[key] = value
+		rest = r
+	}
+
+	return dict, rest, nil
+}
+
+func decodeBencodeValue(buf string) (interface{}, string, error) {
+	if 2 > len(buf) {
+		// todo: this is right?
+		return nil, "", fmt.Errorf("invalid bencoded value, it must have at least two bytes")
+	}
+
+	first := rune(buf[0])
+	switch true {
+	case first == 'l':
+		return decodeList(buf)
+	case first == 'd':
+		return decodeDictionary(buf)
+	case first == 'i':
+		return decodeInteger(buf)
+	case unicode.IsDigit(first):
+		return decodeString(buf)
+	default:
+		return nil, "", fmt.Errorf("unexpected bencoded value format (%s)", buf)
+	}
+}
+
+func decodeBencode(buf string) (interface{}, error) {
+	value, _, err := decodeBencodeValue(buf)
 	return value, err
 }
 
 func main() {
 	command := os.Args[1]
-
 	if command == "decode" {
-		// Uncomment this block to pass the first stage
-		bencodedValue := os.Args[2]
-		decoded, err := decodeBencode(bencodedValue)
+		value := os.Args[2]
+		decoded, err := decodeBencode(value)
 		if err != nil {
 			fmt.Println(err)
 			return
